bbfcd5d544e09fad9a471fbcca153f6be0a56c27
ballenwilli <102811904+ballenwilli@users.noreply.github.com>
2022-04-08 11:09:35 -0500

    Fixed typos in java

--- JS/javascript.js
+++ JS/javascript.js
@@ -30,23 +30,55 @@ if (x === 0) {
   computerSelection = "paper";
 }
 console.log("Computer Selection is:", computerSelection);
-console.log(computerSelection);
 // Display the result.
-let outcome = "null";
-if (playerSelection === "rock" && computerSelection === "scissors") {
-  outcome = "You win! Rock beats Scissors!";
-  wins = wins + 1;
-  console.log(outcome);
-}
-if (playerSelection === "rock" && computerSelection === "paper") {
-  outcome = "You lose! :( Paper covers Rock!)"
-  losses = losses = 1;
-  console.log(outcome);
-}
-if (playerSelection === "rock" && computerSelection === "rock") {
-  outcome = "You tie";
-  ties = ties + 1;
-  console.log(outcome);
+function playRound() {
+  let outcome = "null";
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    outcome = "You win! Rock beats Scissors!";
+    wins = wins + 1;
+    console.log(outcome);
+  }
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    outcome = "You lose! :( Paper covers Rock!)"
+    losses = losses + 1;
+    console.log(outcome);
+  }
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    outcome = "You tie";
+    ties = ties + 1;
+    console.log(outcome);
+  }
+  if (playerSelection === "scissors" && computerSelection === "scissors") {
+    outcome = "You tie";
+    ties = ties + 1;
+    console.log(outcome);
+  }
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+    outcome = "You win! Scissors cuts Paper!";
+    wins = wins + 1;
+    console.log(outcome);
+  }
+  if (playerSelection === "scissors" && computerSelection === "rock") {
+    outcome = "You lose! Rock beats scissors";
+    losses = losses + 1;
+    console.log(outcome);
+  }
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    outcome = "You win! Paper beats Rock!";
+    wins = wins + 1;
+    console.log(outcome);
+  }
+  if (playerSelection === "paper" && computerSelection === "scissors") {
+    outcome = "You lose! :( Scissors beats Rock!)"
+    losses = losses + 1;
+    console.log(outcome);
+  }
+  if (playerSelection === "paper" && computerSelection === "paper") {
+    outcome = "You tie";
+    ties = ties + 1;
+    console.log(outcome);
+  }
+  return(wins, losses, ties);
 }
 function game() {
   for (let p = 0; p <5; p++){
